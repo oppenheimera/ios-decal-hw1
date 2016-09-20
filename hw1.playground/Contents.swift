8fecc6c5f870df29ab2f845ede3f6e2912e28944
Ari Oppenheimer <oppenheimer@berkeley.edu>
2016-09-20 16:14:41 -0700

    finished

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -17,83 +17,89 @@ class Words {
         self.wordA = wordA
         self.wordB = wordB
     }
+    
 
 //: ### Are the values passed in to the **init** function and those set to the instance
 //: ### variables the same type? If not, why?
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: [No, the '!' says that the string is an optional and has a value and that it should be used (they call this forced unwapping), whereas the '?' just says that the value is optional and it's automatically set to nil.]
 
 
 //: ## Q2: Variable Types and Function Types
-    func arePalindromes(_ words: [String]) -> Bool {
-        let reversedWords = words.map() {String($0.characters.reversed())}
-        let numElements = words.count
-        
-        for i in 0 ..< numElements {
-            if words[i] != reversedWords[i] {
-                return false
-            }
-        }
-    }
-//: ### Why does the compiler dislike the **for loop**? Fix it.
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+class func arePalindromes(_ words: [String]) -> Bool {
+    let reversedWords = words.map() {String($0.characters.reversed())}
+    let numElements = words.count
+    
+    for i in 0 ..< numElements {
+        if words[i] != reversedWords[i] {
+            return false
+        }
+    }
+    return true
+}
+
+
+
+//: [The function needed a return statement and also needed to be declared a class function.]
 
 
 //: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
-        var countLetters : [Character : Int] //Line X
-        var lenA = self.wordA.characters.count
-        var lenB = self.wordB.characters.count
-        
-        if lenA != lenB {
-            return false
-        }
-        
-        var arrA = Array(self.wordA.characters)
-        var arrB = Array(self.wordB.characters)
-        
-        for i in 0...lenA-1 {
-            let letter = arrA[i]
-            if let val = countLetters[letter] { //Line Y
-                countLetters[letter] = val + 1
-            } else {
-                countLetters[letter] = 1
-            }
-        }
-        
-        for i in 0...lenB-1 {
-            let letter = arrB[i]
-            if let val = countLetters[letter] {
-                countLetters[letter] = val - 1
-            } else {
-                return false
-            }
-        }
-        
-        for (letter, count) in countLetters {
-            if count != 0 {
-                return false
-            }
-        }
-        
-        return nil
-    }
+
 //: ### What is the problem with declaring **countLetters** as we do in **Line X**,
 //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+func isAnagram() -> Bool {
+    var countLetters = [Character : Int]() //Line X
+    let lenA = self.wordA.characters.count
+    let lenB = self.wordB.characters.count
+    
+    if lenA != lenB {
+        return false
+    }
+    
+    var arrA = Array(self.wordA.characters)
+    var arrB = Array(self.wordB.characters)
+    
+    for i in 0...lenA-1 {
+        let letter = arrA[i]
+        if let val = countLetters[letter] { //Line Y
+            countLetters[letter] = val + 1
+        } else {
+            countLetters[letter] = 1
+        }
+    }
+    
+    for i in 0...lenB-1 {
+        let letter = arrB[i]
+        if let val = countLetters[letter] {
+            countLetters[letter] = val - 1
+        } else {
+            return false
+        }
+    }
+    
+    for (_, count) in countLetters {
+        if count != 0 {
+            return false
+        }
+    }
+    
+    return true
+}
+
+
+//: [The function had to be changed to return a bool. Also the dictionary needed to be instatiated with empty values and the arrays had to be changed to constants.]
     
     
 }
-
 
 //: ## Check Your Answers
 //: ### If you now see an output stream in the right sidebar, you have successfully
@@ -102,8 +108,8 @@ class Words {
 
 
 //: **DO NOT** touch any code below this point.
-Words.arePalindromes(["hih", "racecar", "mom", "wow"])
-Words.arePalindromes(["gene", "shawn", "hello"])
+//Words.arePalindromes(["hih", "racecar", "mom", "wow"])
+//Words.arePalindromes(["gene", "shawn", "hello"])
 var wordsObj = Words.init(wordA: "anagram", wordB: "managra")
 wordsObj.isAnagram()
 wordsObj.wordA = "hello"
